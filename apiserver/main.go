0c83c4ebce4a5a177bb5def59833041be0aa1d18
Pulak Kanti Bhowmick <pulak@appscode.com>
2021-03-05 18:01:21 +0600

    Add simple tls api server

--- apiserver/main.go
+++ apiserver/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/spf13/afero"
 	"github.com/tamalsaha/DIY-k8s-extended-apiserver/lib/certstore"
+	"github.com/tamalsaha/DIY-k8s-extended-apiserver/lib/server"
+	"k8s.io/client-go/util/cert"
 )
 
 func main() {
@@ -17,25 +23,44 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	//serverCert, serverkey, err := store.NewServerCertPair(cert.AltNames{
-	//	IPs: []net.IP{net.ParseIP("127.0.0.1")},
-	//})
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//err = store.Write("tls", serverCert, serverkey)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//clientCert, clientKey, err := store.NewClientCertPair(cert.AltName{
-	//	DNSNames: []string{"john"},
-	//})
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//err = store.write("john", clientCert, clientKey)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//cfg := server.Config{}
+	serverCert, serverKey, err := store.NewServerCertPair(cert.AltNames{
+		IPs: []net.IP{net.ParseIP("127.0.0.1")},
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = store.Write("tls", serverCert, serverKey)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	clientCert, clientKey, err := store.NewClientCertPair(cert.AltNames{
+		DNSNames: []string{"alice"},
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = store.Write("alice", clientCert, clientKey)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	cfg := server.Config{
+		Address: "127.0.0.1:8443",
+		CACertFiles: []string{
+			store.CertFile("ca"),
+		},
+		CertFile: store.CertFile("tls"),
+		KeyFile:  store.KeyFile("tls"),
+	}
+	srv := server.NewGenericServer(cfg)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/core/{resource}", func(res http.ResponseWriter, req *http.Request) {
+		vars := mux.Vars(req)
+		res.WriteHeader(http.StatusOK)
+		fmt.Fprintf(res, "Resource: %v\n", vars["resource"])
+	})
+	r.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(res, "All is good!")
+	})
+	srv.ListenAndServe(r)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/pkbhowmick/k8s-extended-apiserver
 go 1.15
 
 require (
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2
 	github.com/spf13/afero v1.5.1
 	github.com/tamalsaha/DIY-k8s-extended-apiserver v0.0.0-20190115100249-5c40974efc74
+	k8s.io/client-go v9.0.0+incompatible
 )
